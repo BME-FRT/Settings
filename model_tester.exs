b1364a6e963a1670a5d40ce46ae4bde863dbd402
ppeti26 <peti2000426@gmail.com>
2023-09-18 23:28:08 +0200

    model tester template

---
+++ model_tester.exs
@@ -0,0 +1,80 @@
+[General]
+Export type=2
+Include base=1
+Diadem header only=0
+Use phases=1
+Divide by phases=0
+Use event path=0
+Channels type=2
+Spec sheet groups type=0
+Sampling rate index=6
+Interpolate=0
+Distance export=0
+Distance every meter=10
+Use special channel identifier=0
+Export folder=C:\Users\pungorpeter\Desktop
+Export file name=asdasd
+
+[Phase conditions]
+Conditions file=model_tester.pct
+
+[SeparatorSettings]
+ListSeparator=,
+DecimalSeparator=.
+QuotationMarks= 
+
+[Channels]
+AMK1_actual_velocity=1
+AMK2_actual_velocity=1
+AMK3_actual_velocity=1
+AMK4_actual_velocity=1
+APPS1_position=1
+APPS2_posiition=1
+STA1_position=1
+STA2_position=1
+HV_Current=1
+HV_Voltage_After_AIRs=1
+HV_Cell_Voltage=1
+HV_Cell_Temperature=1
+YawRate_Bosch=1
+AccX_Bosch=1
+AccY_Bosch=1
+Brake_Force_sensor=1
+Brake_pressure_front=1
+Brake_pressure_rear=1
+AMK1_temp_motor=1
+AMK2_temp_motor=1
+AMK3_temp_motor=1
+AMK4_temp_motor=1
+AMK1_temp_inverter=1
+AMK2_temp_inverter=1
+AMK3_temp_inverter=1
+AMK4_temp_inverter=1
+VDC_Power_Max=1
+VDC_RPM_Max=1
+VDC_Driving_Torque_Multiplier=1
+VDC_Braking_Torque_Multiplier=1
+VDC_Yaw_Control_On=1
+VDC_Yaw_Control_P=1
+VDC_Yaw_Control_Balance=1
+VDC_Brake_Balance=1
+VDC_Brake_Force_Max=1
+VDC_M_Yaw_Limit=1
+VDC_Powlim_Approx=1
+VDC_Powerlimiter_Hardcore_On=1
+VDC_Powerlimiter_Torque_On=1
+VDC_Slip_Control_On=1
+VDC_Slip_Control_P=1
+VDC_Slip_Control_I=1
+VDC_Slip_Control_D=1
+VDC_Disable_Regbrake_F=1
+VDC_Launch_Control_Mu=1
+VDC_Static_Weight_Distribution=1
+VDC_Optimal_Slip=1
+VDC_Optimal_Slip_Braking=1
+VDC_Drop_Parameter=1
+VDC_Throttle_Breakpoint=1
+VDC_BFS_Offset=1
+VDC_Safety_Mode=1
+VDC_Yaw_Target_Selector=1
+
